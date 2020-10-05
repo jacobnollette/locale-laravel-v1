32837e6d28e9b1a4b658024cb81d5a8e64a19273
Jacob Nollette <jacob@jacobnollette.com>
2020-10-04 20:09:58 -0500

    posts controller

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-/**
- * Route::get('/', function () {
- * return view('welcome');
- * });
- **/
+
 Route::get('posts/{post}', 'App\Http\Controllers\PostsController@show' );
 
 

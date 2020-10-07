da96fb4f7c372e3ce47dee3d04fb6a929ac4dbf0
Jacob Nollette <jacob@jacobnollette.com>
2020-10-06 23:02:20 -0500

    starting to catch response from spotify

--- app/Http/Controllers/SpotifyController.php
+++ app/Http/Controllers/SpotifyController.php
@@ -7,12 +7,55 @@ use Illuminate\Http\Request;
 
 class SpotifyController extends Controller
 {
-  public function index()
-  {
-    return view('spotify/index');
-  }
-  public function response()
-  {
-      return view( 'spotify/response');
-  }
+    private $token = "hello";
+    private $response_url;
+    /**
+     *  Crucial spotify secrets
+     */
+    private $client_id;
+    private $client_secret;
+    private $client_hashed_token;
+
+    function __construct() {
+        $this->response_url = urlencode( 'https://locale.test/spotify/response');
+        $this->client_hashed_token = base64_encode($this->client_id . ':' . $this->client_secret);
+        $this->client_id = env('SPOTIFY_CLIENT_ID');
+        $this->client_secret = env('SPOTIFY_CLIENT_SECRET');
+    }
+
+
+    public function index()
+    {
+
+        return view('spotify/index', [
+            'token'             => $this->token,
+            'client_id'         => $this->client_id,
+            'redirect_url'      => $this->response_url
+        ]);
+    }
+
+//    public function bearer_token(Request $request, $id)
+//    {
+//        //  laravel get requests
+//        //  https://laravel.com/docs/8.x/http-client
+//
+//
+////        $response = Http::withHeaders([
+////            'Authorization' => $this->client_hashed_token,
+////        ])->get('http://test.com/users', [
+////            'name' => 'Taylor',
+////        ]);
+////        $response = Http::get('https://accounts.spotify.com/authorize', [
+////            'client_id'         => $this->client_id,
+////            'response_type'     => 'token',
+////            'redirect_uri'      => urlencode( 'https://locale.test/spotify/response'),
+////        ]);
+//
+//    }
+
+    public function response()
+    {
+        https://locale.test/spotify/response#access_token=BQABFpoO25BaEfWBH7j1-Re6hxpd_NNjks-lhyZYhU0ZVAggDgyBPYqO_lOKiZHzedfZxIs_n5eXjMQUz1LKbaWl-q8nrNejm0ZTxB-v0IUioGsb3WPPIyKp1EvGXM5xhTAD2tk2uqdMxsz2rlg8tLE7n_JPeFuZNdU&token_type=Bearer&expires_in=3600
+        return view('spotify/response');
+    }
 }
--- resources/views/spotify/response.blade.php
+++ resources/views/spotify/response.blade.php
@@ -1,11 +1,20 @@
-@extends("layouts.global")
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
 
-@section("page_title", "Spotify - Login")
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-@section("page_content")
-    <section id="main">
-        <div class="container">
 
-        </div>
-    </section>
-@endsection
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+</head>
+<body>
+<script>
+
+    var type = window.location.hash.substr(1);
+    console.log(type);
+</script>
+</body>
+</html>

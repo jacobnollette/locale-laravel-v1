3d60a01d603483c86862018a9aa6f6806ed5ed8e
Jacob Nollette <jacob@jacobnollette.com>
2020-10-06 19:12:20 -0500

    basics layout stuff

--- resources/views/layouts/global.blade.php
+++ resources/views/layouts/global.blade.php
@@ -7,11 +7,14 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield("page_title")</title>
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Oleo+Script:wght@700&family=Poppins:ital,wght@0,100;0,400;1,400&display=swap" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
 </head>
 <body>
+@include('layouts.header')
+
+
 @yield("page_content")
 </body>
 
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,3 @@
+<header><div id="header_container" class="container">
+    <div id="header_logo"><a href="/">Music Locale</a></div>
+</div></header>

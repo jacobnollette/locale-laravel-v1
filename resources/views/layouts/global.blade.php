c9ed20a7c67436013b37d3c5b8b0b2b56379655b
Jacob Nollette <jacob@jacobnollette.com>
2020-10-06 19:41:01 -0500

    moar styles

--- resources/views/layouts/global.blade.php
+++ resources/views/layouts/global.blade.php
@@ -7,7 +7,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield("page_title")</title>
-    <link href="https://fonts.googleapis.com/css2?family=Oleo+Script:wght@700&family=Poppins:ital,wght@0,100;0,400;1,400&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Oleo+Script:wght@700&family=Poppins:ital,wght@0,200;0,400;1,400&display=swap" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
 </head>
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,9 +1,12 @@
 @extends("layouts.global")
 
-@section("page_title", "Spotify")
+@section("page_title", "Spotify - Demo")
 
 @section("page_content")
+<section id="main"><div class="container">
     {{ $post->body }}
+</div></section>
+
 @endsection
 
 

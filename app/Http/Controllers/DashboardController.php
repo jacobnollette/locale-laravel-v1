255ccf1ec2d7745be515156c0161519ddf2f0dcf
Jacob Nollette <jacob@jacobnollette.com>
2020-10-08 16:22:05 -0500

    needing to work through the spotify token, or refresh problems.

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,9 +6,16 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 use App\Models\User;
-
 use App\Models\Dashboard;
 use App\Models\Spotify;
+
+/**
+ * packages
+ */
+
+//  spotify api wrapper - https://github.com/jwilsson/spotify-web-api-php#usage
+use SpotifyWebAPI\SpotifyWebAPI;
+use SpotifyWebAPI\Session;
 
 class DashboardController extends Controller
 {
@@ -22,19 +29,47 @@ class DashboardController extends Controller
     private $client_secret;
     private $client_hashed_token;
 
+    private $spotify_session;
+    private $spotify_api;
+
 
     function __construct()
     {
-        $this->response_url = urlencode('https://locale.test/spotify/response');
+        $this->response_url_raw = 'https://locale.test/spotify/auth/response';
+        $this->response_url = urlencode($this->response_url_raw);
         $this->client_hashed_token = base64_encode($this->client_id . ':' . $this->client_secret);
         $this->client_id = env('SPOTIFY_CLIENT_ID');
         $this->client_secret = env('SPOTIFY_CLIENT_SECRET');
+
+        $this->spotify_session = new Session(
+            env("SPOTIFY_CLIENT_ID"),
+            env("SPOTIFY_CLIENT_SECRET"),
+            $this->response_url_raw
+        );
+
+
+        /**
+         * Spotify access token
+         * initialize spotify web api token
+         */
+        $user_id = Auth::id();
+        $_user = User::where("id", "=", $user_id)->first();
+        if (isset($_user->spotify_access_token)):
+            $_user_access_token = $_user->spotify_access_token;
+            $this->spotify_api = new SpotifyWebAPI();
+            $this->spotify_api->setAccessToken($_user_access_token);
+        endif;
     }
 
-    public function index() {
+    public function index()
+    {
+
+
+        print_r($this->spotify_api->getTrack('7EjyzZcbLxW7PaaLua9Ksb'));
+
 
         //$token = Spotify::get_user_access_token();
-       // $bearer_token = Spotify::authorize_bearer_token();
+        // $bearer_token = Spotify::authorize_bearer_token();
 
         //echo $bearer_token;
         //die();
--- app/Http/Middleware/Spotify.php
+++ app/Http/Middleware/Spotify.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 
 use App\Models\User;
@@ -15,8 +16,8 @@ class Spotify
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
      * @return mixed
      */
 
@@ -27,7 +28,7 @@ class Spotify
     function __construct()
     {
         $this->response_url_raw = 'https://locale.test/spotify/auth/response';
-        $this->response_url = urlencode( $this->response_url_raw );
+        $this->response_url = urlencode($this->response_url_raw);
     }
 
 
@@ -39,7 +40,27 @@ class Spotify
             $this->response_url_raw
         );
 
-//
+
+        $user_id = Auth::id();
+        if (isset ($user_id)):
+            $_user = User::where("id", "=", $user_id)->first();
+            $_user_access_token = $_user->spotify_access_token;
+            if (isset ($_user_access_token)):
+
+                //echo $_user_access_token;
+                //die();
+                //  refresh user token & save it to the database
+                $new_access_token = $session->refreshAccessToken($_user_access_token);
+                User::where("id", "=", $user_id)->update(array(
+                    'spotify_access_token' => $new_access_token,
+                    'spotify_access_token_added' => now()
+                ));
+
+            endif;
+        endif;
+
+        // die();
+
 //        $session->refreshAccessToken($refreshToken);
 //
 //        $accessToken = $session->getAccessToken();

3da13ea3492ff1887ed7d3a6be94d6c7c5288858
Jacob Nollette <jacob@jacobnollette.com>
2020-10-14 18:54:14 -0500

    initialized explorer controller

---
+++ app/Http/Controllers/ExplorerController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+
+/**
+ * controllers
+ */
+
+use App\Http\Controllers\SpotifyController;
+
+/**
+ * models
+ */
+
+use App\Models\User;
+use App\Models\Dashboard;
+use App\Models\Spotify;
+use App\Models\Spotify_playlists;
+use App\Models\User_crates;
+
+/**
+ * packages
+ */
+
+//  spotify api wrapper - https://github.com/jwilsson/spotify-web-api-php#usage
+use SpotifyWebAPI\SpotifyWebAPI;
+use SpotifyWebAPI\Session;
+
+class ExplorerController extends Controller
+{
+    /**
+     * Modules
+     */
+    private $spotify;
+
+
+    function __construct()
+    {
+        $this->spotify = new SpotifyController();
+    }
+}

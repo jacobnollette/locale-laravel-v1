11e90bd1760d42289e3c56a08c268291617a7fb4
Jacob Nollette <jacob@jacobnollette.com>
2020-10-21 00:21:53 -0500

    styled the track listing

--- resources/views/playlists/edit.blade.php
+++ resources/views/playlists/edit.blade.php
@@ -4,26 +4,29 @@
 
 @section("page_content")
     <section id="main">
-        <div class="container">
+        <div id="playlists_edit" class="container">
             <h1>{{$playlist->name}}</h1>
             <div class="playlists_edit-description">{{$playlist->description}}</div>
             <div class="playlists_edit-tracks">
+                <ul id="playlists_edit-tracks-list">
                 @foreach ( $playlist->tracks->items as $track )
-                    <div class="playlists_edit-tracks-track">
-                        <div class="playlists_edit-tracks-title">
+                    <li class="playlists_edit-tracks-track" data-id="{{$track->track->id}}">
+                        <div class="playlists_edit-tracks-title" >
                             {{$track->track->name}}
                         </div>
+                        <div class="playlists_edit-artist_break">&mdash;</div>
                         <div class="playlists_edit-tracks-artists">
                             <div class="playlists_edit-tracks-artists-display">Artists:</div>
                             <ul>
                                 @foreach( $track->track->artists as $artist )
-                                    <li>{{$artist->name}}</li>
+                                    <li><a href="https://open.spotify.com/artist/{{$artist->id}}">{{$artist->name}}</a></li>
                                 @endforeach
                             </ul>
                         </div>
                         <!-- {{$track->track->album->name}} -->
-                    </div>
+                    </li>
                 @endforeach
+                </ul>
 
             </div>
         </div>

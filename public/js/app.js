9a3537b6089a9b62bcacee043edd0ce0aa31af87
Jacob Nollette <jacob@jacobnollette.com>
2020-10-22 15:40:46 -0500

    planning on rebuilding javascript

--- public/js/app.js
+++ public/js/app.js
@@ -197,12 +197,21 @@ if ($("#explorer_index").length > 0) {
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-(function ($) {})(jQuery);
+// (function($) {
+//
+// })(jQuery);
+// $( document ).ready(function() {
+//     /*
+//         initialize slider
+//     */
+//     var _home_attorneys_slider_actual = Object.create( _home_attorneys_slider );
+//     _home_attorneys_slider_actual.slider_init();
+//
+// });
+
 /**
  * ghetto iife
  */
-
-
 if ($("#playlists_edit").length > 0) {
   playlist_edit_map();
 }
--- resources/js/blocks/playlist_edit.js
+++ resources/js/blocks/playlist_edit.js
@@ -1,9 +1,18 @@
 
 
 
-(function($) {
+// (function($) {
+//
+// })(jQuery);
+// $( document ).ready(function() {
+//     /*
+//         initialize slider
+//     */
+//     var _home_attorneys_slider_actual = Object.create( _home_attorneys_slider );
+//     _home_attorneys_slider_actual.slider_init();
+//
+// });
 
-})(jQuery);
 /**
  * ghetto iife
  */

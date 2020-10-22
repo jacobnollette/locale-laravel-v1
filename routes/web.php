d6d280c9973ffc454db0d098658a3116dd64c14e
Jacob Nollette <jacob@jacobnollette.com>
2020-10-22 15:34:26 -0500

    cleaned up second half of the routes

--- routes/web.php
+++ routes/web.php
@@ -21,38 +21,38 @@ Auth::routes();
 /**
  * Dashboard rubbish
  */
-Route::get('/dashboard/', 'App\Http\Controllers\DashboardController@index');
-Route::post('/dashboard/playlist/add/', 'App\Http\Controllers\DashboardController@playlist_add');
-Route::post('/dashboard/playlist/remove/', 'App\Http\Controllers\DashboardController@playlist_remove');
+Route::get('/dashboard/','App\Http\Controllers\DashboardController@index');
+Route::post('/dashboard/playlist/add/','App\Http\Controllers\DashboardController@playlist_add');
+Route::post('/dashboard/playlist/remove/','App\Http\Controllers\DashboardController@playlist_remove');
 
 /**
  * Playlist rubbish
  */
-Route::get('/playlist/{id}/', 'App\Http\Controllers\PlaylistController@index');
-Route::post('/playlist/{id}/update/', 'App\Http\Controllers\PlaylistController@update');
+Route::get('/playlist/{id}/','App\Http\Controllers\PlaylistController@index');
+Route::post('/playlist/{id}/update/','App\Http\Controllers\PlaylistController@update');
 
 /**
  * Location API rubbish
  */
-Route::post('/utility/location/get/', 'App\Http\Controllers\LocationAPIController@api_v1_location_get');
+Route::post('/utility/location/get/','App\Http\Controllers\LocationAPIController@api_v1_location_get');
 
 /**
  * Explorer rubbish
  */
-Route::get('/explore/', 'App\Http\Controllers\ExplorerController@index');
-Route::post( '/dashboard/explore/add/', 'App\Http\Controllers\ExplorerController@explorer_add');
-Route::post( '/dashboard/explore/remove/', 'App\Http\Controllers\ExplorerController@explorer_remove');
+Route::get('/explore/','App\Http\Controllers\ExplorerController@index');
+Route::post( '/dashboard/explore/add/','App\Http\Controllers\ExplorerController@explorer_add');
+Route::post( '/dashboard/explore/remove/','App\Http\Controllers\ExplorerController@explorer_remove');
 
 /**
  * Spotify rubbish
  */
-Route::get('/spotify/', 'App\Http\Controllers\SpotifyController@index');
+Route::get('/spotify/','App\Http\Controllers\SpotifyController@index');
 Route::get('/spotify/auth/response', 'App\Http\Controllers\SpotifyController@spotify_auth_response');
-Route::get('/spotify/auth', 'App\Http\Controllers\SpotifyController@spotify_auth_get_redirect');
-Route::post('/spotify/input', 'App\Http\Controllers\SpotifyController@input');
+Route::get('/spotify/auth','App\Http\Controllers\SpotifyController@spotify_auth_get_redirect');
+Route::post('/spotify/input','App\Http\Controllers\SpotifyController@input');
 
 /**
  * Home rubbish
  */
-Route::get( '/', 'App\Http\Controllers\HomeController@landing');
-Route::get( '/home', 'App\Http\Controllers\HomeController@index');
+Route::get( '/','App\Http\Controllers\HomeController@landing');
+Route::get( '/home','App\Http\Controllers\HomeController@index');

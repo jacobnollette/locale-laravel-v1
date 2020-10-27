a4d4da9d554c6de629ae95c6e5b04d3213586c33
Jacob Nollette <jacob@jacobnollette.com>
2020-10-27 14:45:03 -0500

    migration WIP

--- database/migrations/2020_10_27_031552_convert_locations_to_points.php
+++ database/migrations/2020_10_27_031552_convert_locations_to_points.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+use Grimzy\LaravelMysqlSpatial\Types\Point;
+
 class ConvertLocationsToPoints extends Migration
 {
     /**
@@ -13,21 +15,20 @@ class ConvertLocationsToPoints extends Migration
      */
     public function up()
     {
-        Schema::table('spotify_playlists', function (Blueprint $table) {
-            $table->point("location")->nullable();
-        });
+//        Schema::table('spotify_playlists', function (Blueprint $table) {
+//            $table->point("location")->nullable();
+//        });
 
-        $results = DB::table('spotify_playlists')->select('id')->get();
+        $results = DB::table('spotify_playlists')->select('id',"location_lat", "location_long")->get();
 
         $i = 1;
         foreach ($results as $result){
+
+            $_point = new Point( $result->location_lat, $result->location_long );
             DB::table('spotify_playlists')
                 ->where('id',$result->id)
                 ->update([
-                    "location" => array(
-                        $result->location_lat,
-                        $result->location_long
-                    )
+                    "location" => $_point
                 ]);
             $i++;
         }

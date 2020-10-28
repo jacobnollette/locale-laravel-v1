b96db1fdf83cf050e1a67585389fecb96ba25ac0
Jacob Nollette <jacob@jacobnollette.com>
2020-10-28 18:32:40 -0500

    playlist file name

--- database/migrations/2020_10_08_235749_add_playlist.php
+++ database/migrations/2020_10_08_235749_add_playlist.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddPlaylists extends Migration
+class AddPlaylist extends Migration
 {
     /**
      * Run the migrations.

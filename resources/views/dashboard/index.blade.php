20039dc679ea6a0d6e58d9ea043f4dad2c21a178
Jacob Nollette <jacob@jacobnollette.com>
2020-10-29 13:56:33 -0500

    dashboard index

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -12,6 +12,7 @@
                 @foreach ($playlists as $playlist)
                     <div class="playlist" data-playlist_id="{{$playlist->id}}">
                         <div class="playlist_image"><img src="{{$playlist->images[0]->url}}"/></div>
+                        <div class="clear">&nbsp;</div>
                         <div class="playlist_display">
                             <div class="playlist_name">
                                 <a href="/playlist/{{$playlist->id}}">{{$playlist->name}}</a>

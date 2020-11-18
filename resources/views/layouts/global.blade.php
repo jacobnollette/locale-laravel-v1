3470cab7b5ba796ba2d06ccd148dc814328e5a35
Jacob Nollette <jacob@jacobnollette.com>
2020-11-17 21:08:07 -0600

    bumped build number

--- resources/views/layouts/global.blade.php
+++ resources/views/layouts/global.blade.php
@@ -10,7 +10,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield("page_title")</title>
     <link href="https://fonts.googleapis.com/css2?family=Oleo+Script:wght@700&family=Poppins:ital,wght@0,200;0,400;1,400&display=swap" rel="stylesheet">
-    <link href="{{ asset('css/app.css') }}?v=0.0.1" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}?v=0.0.1.1" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script src="https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js"></script>
 
@@ -21,5 +21,5 @@
 
 @yield("page_content")
 </body>
-<script src="{{ asset('js/app.js') }}?v=0.0.1" defer></script>
+<script src="{{ asset('js/app.js') }}?v=0.0.1.1" defer></script>
 </html>

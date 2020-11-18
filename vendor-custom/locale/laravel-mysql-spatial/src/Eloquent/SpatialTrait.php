2383f18ae6edb7f35e3ffd1af80cf630129d33b5
Jacob Nollette <jacob@jacobnollette.com>
2020-11-17 21:33:38 -0600

    spatial trait

---
+++ vendor-custom/locale/laravel-mysql-spatial/src/Eloquent/SpatialTrait.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Grimzy\LaravelMysqlSpatial\Eloquent;
+
+use Illuminate\Database\Query\Expression;
+
+/**
+ * Removes the the ST_GeomFromText 'axis-order=long-lat' argument that fails on MariaDB.
+ */
+class SpatialTrait extends Expression
+{
+    public function getValue()
+    {
+        return "ST_GeomFromText(?, ?)";
+    }
+
+    public function getSpatialValue()
+    {
+        return $this->value->toWkt();
+    }
+
+    public function getSrid()
+    {
+        return $this->value->getSrid();
+    }
+}
+

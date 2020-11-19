3db23a50aa4b4ffa94c7e165652126e427416adb
Jacob Nollette <jacob@jacobnollette.com>
2020-11-18 22:23:10 -0600

    logged in landing page, revamp

--- resources/views/home/landing.blade.php
+++ resources/views/home/landing.blade.php
@@ -5,21 +5,25 @@
 @section("page_content")
     <section id="main">
         <div class="container">
+            <div id="home_landing">
 
+                <h1>Welcome</h1>
+                @auth
+                    <div id="home_landing-display">
+                        <div id="home_landing-display_explore">
+                            <h2><a href="/explore">Explore and find new Music</a></h2></div>
+                        <div id="home_landing-display_plus"><h2>+</h2></div>
+                        <div id="home_landing-display_dashboard">
+                            <h2><a href="/dashboard">Share music from the Dashboard</a></h2></div>
+                    </div>
+                @else
+                    <h2>Please register my account <br\> (in the upper right)</h2>
+                    <p>Music Locale requires a Spotify account, and the ability to modify playlists. Playlists which
+                        users unlock will be added to their account. Existing playlists will not be modified, only
+                        shared per the users request.</p>
+                @endif
 
-
-
-            <h1>Welcome</h1>
-            @auth
-                <h2>Explore and find new <a href="/explore">Music</a></h2>
-                <h2>Share music from the <a href="/dashboard">Dashboard</a></h2>
-
-            @else
-                <h2>Please register my account <br\> (in the upper right)</h2>
-                <p>Music Locale requires a Spotify account, and the ability to modify playlists. Playlists which users unlock will be added to their account. Existing playlists will not be modified, only shared per the users request.</p>
-            @endif
-
-
+            </div>
         </div>
     </section>
 @endsection

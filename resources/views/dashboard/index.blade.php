93ab0d101f1d20d4208fc384b7f09251a48de2b8
Jacob Nollette <jacob@jacobnollette.com>
2020-12-03 20:22:38 -0600

    bang

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -9,18 +9,20 @@
             <p>If you see playlists in your dashboard, your Spotify account is connected!</p>
 
             <div id="playlists_index">
-                @foreach ($playlists as $playlist)
-                    <div class="playlist" data-playlist_id="{{$playlist->id}}">
-                        <div class="playlist_image"><img src="{{$playlist->images[0]->url}}"/></div>
-                        <div class="clear">&nbsp;</div>
-                        <div class="playlist_display">
-                            <div class="playlist_name">
-                                <a href="/playlist/{{$playlist->id}}">{{$playlist->name}}</a>
+                @if ($playlists)
+                    @foreach ($playlists as $playlist)
+                        <div class="playlist" data-playlist_id="{{$playlist->id}}">
+                            <div class="playlist_image"><img src="{{$playlist->images[0]->url}}"/></div>
+                            <div class="clear">&nbsp;</div>
+                            <div class="playlist_display">
+                                <div class="playlist_name">
+                                    <a href="/playlist/{{$playlist->id}}">{{$playlist->name}}</a>
+                                </div>
                             </div>
+                            <div class="clear">&nbsp;</div>
                         </div>
-                        <div class="clear">&nbsp;</div>
-                    </div>
-                @endforeach
+                    @endforeach
+                @endif
             </div>
         </div>
     </section>
